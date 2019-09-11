01f73524f74733d734fe967aa1575b651b90630c
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-09-11 13:14:36 +0200

    fix: support process.env.AWS_DEFAULT_REGION

--- src/lifecycle/fetch-stack-configuration.ts
+++ src/lifecycle/fetch-stack-configuration.ts
@@ -3,7 +3,9 @@ import { CloudFormation } from 'aws-sdk'
 export const fetchStackConfiguration = async (
 	StackName: string,
 ): Promise<{ [key: string]: string }> => {
-	const cf = new CloudFormation()
+	const cf = new CloudFormation({
+		region: process.env.AWS_DEFAULT_REGION,
+	})
 	const { Stacks } = await cf.describeStacks({ StackName }).promise()
 	if (!Stacks) {
 		throw new Error(`Unknown stack "${StackName}"!`)
