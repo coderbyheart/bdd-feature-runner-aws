705ae7340a9009acf6f0bd1788eac6f4bfb9cb07
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-09-23 17:32:41 +0200

    feat: add generic storage steps

---
+++ src/steps/storage.ts
@@ -0,0 +1,23 @@
+import * as jsonata from 'jsonata'
+import { expect } from 'chai'
+import { regexGroupMatcher } from '../lib/regexGroupMatcher'
+import { StepRunner } from '../lib/runner'
+
+export const storageStepRunners = (): StepRunner<any>[] => [
+	regexGroupMatcher(
+		/^(?:"(?<exp>[^"]+)" of )?"(?<storeName>[^"]+)" (?<equalOrMatch>equal|match) this JSON$/,
+	)(async ({ exp, equalOrMatch, storeName }, step, runner) => {
+		if (!step.interpolatedArgument) {
+			throw new Error('Must provide argument!')
+		}
+		const j = JSON.parse(step.interpolatedArgument)
+		const result = runner.store[storeName]
+		const fragment = exp ? jsonata(exp).evaluate(result) : result
+		if (equalOrMatch === 'match') {
+			expect(fragment).to.containSubset(j)
+		} else {
+			expect(fragment).to.deep.equal(j)
+		}
+		return [fragment]
+	}),
+]
