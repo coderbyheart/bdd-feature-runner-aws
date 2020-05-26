fba9abbf680cffcf1253159a3693f2d11539b516
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-05-26 23:52:49 +0200

    refactor: get webhook endpoint from arguments
    BREAKING CHANGE: the webhookQueue must be passed as an argument
    
    Previously it was set through the World, but it is
    unlikely that a test runner has multiple webhook
    receivers, and switching their URL between tests
    through the store is not very straightforward.

--- src/steps/webhook.ts
+++ src/steps/webhook.ts
@@ -1,7 +1,6 @@
 import * as jsonata from 'jsonata'
 import * as chai from 'chai'
 import { expect } from 'chai'
-import { Store } from '../lib/runner'
 import { regexMatcher } from '../lib/regexMatcher'
 import { WebhookReceiver } from '../lib/webhook-receiver'
 import * as chaiSubset from 'chai-subset'
@@ -9,60 +8,59 @@ import { regexGroupMatcher } from '../lib/regexGroupMatcher'
 
 chai.use(chaiSubset)
 
-let r: WebhookReceiver
-
-type WebhookStepRunnersWorld = Store & {
-	webhookQueue: string
-}
-
-export const webhookStepRunners = <W extends WebhookStepRunnersWorld>({
+export const webhookStepRunners = ({
 	region,
+	webhookQueue,
 }: {
 	region: string
-}) => [
-	regexMatcher<W>(
-		/^the Webhook Receiver "([^"]+)" should be called$/,
-	)(async ([MessageGroupId], _, runner) =>
-		r.receiveWebhookRequest(MessageGroupId, runner).then((r) => r.body),
-	),
-	regexMatcher<W>(
-		/^"([^"]+)" of the webhook request body should equal "([^"]+)"$/,
-	)(async ([exp, expected]) => {
-		const e = jsonata(exp)
-		expect(r.latestWebhookRequest).not.to.be.an('undefined')
-		const b = r.latestWebhookRequest?.body
-		expect(b).not.to.be.an('undefined')
-		const result = e.evaluate(b)
-		expect(result).to.deep.equal(expected)
-		return b
-	}),
-	regexMatcher<W>(/^the webhook request body should (equal|match) this JSON$/)(
-		async ([equalOrMatch], step) => {
-			if (!step.interpolatedArgument) {
-				throw new Error('Must provide argument!')
-			}
-			const j = JSON.parse(step.interpolatedArgument)
+	webhookQueue: string
+}) => {
+	let r: WebhookReceiver
+	return [
+		regexMatcher(
+			/^the Webhook Receiver "([^"]+)" should be called$/,
+		)(async ([MessageGroupId], _, runner) =>
+			r.receiveWebhookRequest(MessageGroupId, runner).then((r) => r.body),
+		),
+		regexMatcher(
+			/^"([^"]+)" of the webhook request body should equal "([^"]+)"$/,
+		)(async ([exp, expected]) => {
+			const e = jsonata(exp)
+			expect(r.latestWebhookRequest).not.to.be.an('undefined')
 			const b = r.latestWebhookRequest?.body
 			expect(b).not.to.be.an('undefined')
-			if (equalOrMatch === 'match') {
-				expect(b).to.containSubset(j)
-			} else {
-				expect(b).to.deep.equal(j)
-			}
+			const result = e.evaluate(b)
+			expect(result).to.deep.equal(expected)
 			return b
-		},
-	),
-	regexMatcher<W>(/^I have a Webhook Receiver/)(async (_, __, runner) => {
-		r = new WebhookReceiver(runner.world.webhookQueue, region)
-		await r.clearQueue()
-	}),
-	regexGroupMatcher(
-		/^I store "(?<exp>[^"]+)" of the last webhook request into "(?<storeName>[^"]+)"$/,
-	)(async ({ exp, storeName }, _, runner) => {
-		const e = jsonata(exp)
-		const result = e.evaluate(r.latestWebhookRequest)
-		expect(result).to.not.be.an('undefined')
-		runner.store[storeName] = result
-		return result
-	}),
-]
+		}),
+		regexMatcher(/^the webhook request body should (equal|match) this JSON$/)(
+			async ([equalOrMatch], step) => {
+				if (!step.interpolatedArgument) {
+					throw new Error('Must provide argument!')
+				}
+				const j = JSON.parse(step.interpolatedArgument)
+				const b = r.latestWebhookRequest?.body
+				expect(b).not.to.be.an('undefined')
+				if (equalOrMatch === 'match') {
+					expect(b).to.containSubset(j)
+				} else {
+					expect(b).to.deep.equal(j)
+				}
+				return b
+			},
+		),
+		regexMatcher(/^I have a Webhook Receiver/)(async () => {
+			r = new WebhookReceiver(webhookQueue, region)
+			await r.clearQueue()
+		}),
+		regexGroupMatcher(
+			/^I store "(?<exp>[^"]+)" of the last webhook request into "(?<storeName>[^"]+)"$/,
+		)(async ({ exp, storeName }, _, runner) => {
+			const e = jsonata(exp)
+			const result = e.evaluate(r.latestWebhookRequest)
+			expect(result).to.not.be.an('undefined')
+			runner.store[storeName] = result
+			return result
+		}),
+	]
+}
